6a742eb4141e827edbaae46fad40c3e679325545
Philipp A <flying-sheep@web.de>
2016-01-27 15:52:08 +0100

    consistently use single quotes

--- test_ir.py
+++ test_ir.py
@@ -2,11 +2,11 @@ import unittest
 import jupyter_kernel_test as jkt
 
 class IRkernelTests(jkt.KernelTests):
-    kernel_name = "ir"
+    kernel_name = 'ir'
 
-    language_name = "R"
+    language_name = 'R'
 
-    code_hello_world = "print('hello, world')"
+    code_hello_world = 'print("hello, world")'
 
     completion_samples = [
         {
@@ -15,16 +15,16 @@ class IRkernelTests(jkt.KernelTests):
         },
     ]
 
-    complete_code_samples = ['1', "print('hello, world')", "f <- function(x) {\n  x*2\n}"]
-    incomplete_code_samples = ["print('hello", "f <- function(x) {\n  x*2"]
+    complete_code_samples = ['1', 'print("hello, world")', 'f <- function(x) {\n  x*2\n}']
+    incomplete_code_samples = ['print("hello', 'f <- function(x) {\n  x*2']
 
 class InstallspecTests(jkt.KernelTests):
     # just a small test, it's the same kernel after all...
-    kernel_name = "testir"
+    kernel_name = 'testir'
 
-    language_name = "R"
+    language_name = 'R'
 
-    code_hello_world = "print('hello, world')"
+    code_hello_world = 'print("hello, world")'
 
 
 if __name__ == '__main__':
