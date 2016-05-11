5ec6fdf56a79c163ca530a445a65c5ed9b343cfa
Jan Schulz <jasc@gmx.net>
2016-05-11 12:57:46 +0200

    Fix Travis not running tests

--- .travis-build.sh
+++ .travis-build.sh
@@ -21,10 +21,27 @@ travis_fold start Installing
  R CMD INSTALL "$PKG_TARBALL"
 travis_fold end Installing
 
+
+travis_fold start Python_env
+ bootstrap_header 'Installing Python Environment for testing'
+ # http://conda.pydata.org/docs/travis.html
+ wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+ bash miniconda.sh -b -p $HOME/miniconda
+ export PATH="$HOME/miniconda/bin:$PATH"
+ hash -r
+ conda config --set always_yes yes --set changeps1 no
+ conda update -q conda
+ #Useful for debugging any issues with conda
+ conda info -a
+ # Replace dep1 dep2 ... with your dependencies
+ conda create -q -n test-environment python=3.5 jupyter nose
+ source activate test-environment
+ pip install jupyter_kernel_test
+travis_fold end Python_env
+
 travis_fold start Testing
  bootstrap_header 'Testing package'
  Rscript -e "IRkernel::installspec()"
  Rscript -e "IRkernel::installspec(name = 'testir', displayname = 'testir')"
- easy_install3 jupyter_kernel_test
- python3 test_ir.py
+ python test_ir.py
 travis_fold end Testing
