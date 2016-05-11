5869fc9d896cd71d1bd81fcc1f034cbea4f5ac17
Jan Schulz <jasc@gmx.net>
2016-05-11 13:50:02 +0200

    Test on multiple versions and multiple dependencies

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/bin/bash  
+# error on first problem
+set -e 
 ANSI_YELLOW="\033[33;1m"
 
 bootstrap_header() {
   echo -e "${ANSI_YELLOW}$1${ANSI_RESET}"
 }
+
+if [ "${DEPS}" == "github" ]; then
+ travis_fold start github_dependencies
+  bootstrap_header 'Installing additional github dependencies'
+  Rscript -e "devtools::install_github('irkernel/repr')"
+  Rscript -e "devtools::install_github('irkernel/IRdisplay')"
+ travis_fold end github_dependencies
+fi
 
 travis_fold start Building
  bootstrap_header 'Building package'
@@ -13,14 +23,14 @@ travis_fold end Building
 travis_fold start Checking
  bootstrap_header 'Checking package' 
  export PKG_TARBALL=$(Rscript -e "with(as.list(read.dcf('DESCRIPTION')[1, ]), cat(sprintf('%s_%s.tar.gz', Package, Version)))")
- R CMD check "$PKG_TARBALL"
+ R CMD check $PKG_TARBALL # could also use --as-cran
+ export CHECK_LOG=/home/travis/build/IRkernel/IRkernel/IRkernel.Rcheck/00check.log
 travis_fold end Checking
 
 travis_fold start Installing
  bootstrap_header 'Installing package'
  R CMD INSTALL "$PKG_TARBALL"
 travis_fold end Installing
-
 
 travis_fold start Python_env
  bootstrap_header 'Installing Python Environment for testing'
@@ -43,5 +53,6 @@ travis_fold start Testing
  bootstrap_header 'Testing package'
  Rscript -e "IRkernel::installspec()"
  Rscript -e "IRkernel::installspec(name = 'testir', displayname = 'testir')"
- python test_ir.py
+ python -W ignore::DeprecationWarning test_ir.py
 travis_fold end Testing
+set +e
