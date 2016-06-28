bac6e2e8a242756ae86e58f0ef9a49780a39b947
Jan Schulz <jasc@gmx.net>
2016-06-28 20:13:31 +0200

    Do not compute text/plain twice
    The default mime types includes text/plain so by iterating over that list we
    compute text/plain content twice. as this can be expensive, test for this and
    don't call it again...

--- R/handlers.r
+++ R/handlers.r
@@ -2,12 +2,17 @@ prepare_mimebundle <- function(obj, handle_display_error = log_error) {
     data <- namedlist()
     metadata <- namedlist()
     
+    # we always send text/plain, even if the user removed that from the option!
     data[['text/plain']] <- text_repr <- repr_text(obj)
     
     # Only send a response when there is regular console output
     if (!is.null(text_repr) && nchar(text_repr) > 0L) {
         if (getOption('jupyter.rich_display')) {
             for (mime in getOption('jupyter.display_mimetypes')) {
+                if (mime == 'text/plain') {
+                    # doing a print(obj) can be expensive and we don't want to do it twice!
+                    next
+                }
                 # Use withCallingHandlers as that shows the inner stacktrace:
                 # https://stackoverflow.com/questions/15282471/get-stack-trace-on-trycatched-error-in-r
                 # the tryCatch is  still needed to prevent the error from showing
