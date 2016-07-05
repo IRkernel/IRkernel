0e57a3e5e42e0cfa68c11a71e6ee5212019c90f7
Thomas Kluyver <thomas@kluyver.me.uk>
2016-07-05 14:47:42 +0100

    Simplify test machinery to run Python tests from R

--- tests/testthat/test_kernel.r
+++ tests/testthat/test_kernel.r
@@ -1,29 +1,6 @@
 context('kernel')
 
-test_con <- pipe('python3 test_runner.py', 'rb', 'utf-8')
-if (!isOpen(test_con)) open(test_con)
-
-while(isOpen(test_con)) {
-    test_result <- readBin(test_con, 'character', 1L)
-    if (length(test_result) == 0) break 
-    
-    parts <- strsplit(test_result, '\n')[[1]]
-    if (length(parts) < 2) {
-        test_that('test output parsing', fail(sprintf('needs 2 parts, but only has one: %s', dQuote(parts))))
-        next
-    }
-    
-    name   <- parts[[1]]
-    status <- parts[[2]]
-    msg    <- paste(parts[-(1:2)], collapse = '\n')
-    
-    if (nchar(msg) == 0)
-        msg <- status
-    
-    test_that(name, switch(
-        status,
-        ok =, 'expected failure' = succeed('Success!'),
-        failure =, error =, 'unexpected success' = fail(msg),
-        skipped = skip(msg),
-        stop('unknown status: ', status)))
-}
+status <- system2('python3', 'test_ir.py', wait=TRUE)
+test_that('kernel tests pass', {
+    expect_equal(status, 0)
+})
