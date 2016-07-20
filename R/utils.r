c25a69c8672c0379cdf2490cb2d98595c229e52c
Philipp A <flying-sheep@web.de>
2016-07-20 23:33:01 +0200

    pkg:::.private is apparently ok for CHECK

--- R/utils.r
+++ R/utils.r
@@ -1,3 +1,5 @@
+ellip_h <- repr:::.char_fallback('\u22EF', '...')
+
 #' @importFrom utils head tail
 skip_repeated <- function(vec) {
     enc <- rle(vec)
@@ -7,9 +9,8 @@ skip_repeated <- function(vec) {
         vec
     } else {
         v <- enc$values[[i]]
-        e <- repr:::ellip.h
-        enc$lengths <- c(head(enc$lengths, i - 1), 1, 1, 1, tail(enc$lengths, -i))
-        enc$values  <- c(head(enc$values,  i - 1), v, e, v, tail(enc$values,  -i))
+        enc$lengths <- c(head(enc$lengths, i - 1), 1, 1,       1, tail(enc$lengths, -i))
+        enc$values  <- c(head(enc$values,  i - 1), v, ellip_h, v, tail(enc$values,  -i))
         inverse.rle(enc)
     }
 }
--- tests/testthat/test_utils.r
+++ tests/testthat/test_utils.r
@@ -2,7 +2,7 @@ context('utils')
 
 test_that('skip_repeated works', {
     stack <- c('foo()', 'f()', 'f()', 'f()', 'f()', 'bar()')
-    expect_equal(skip_repeated(stack), c('foo()', 'f()', repr:::ellip.h, 'f()', 'bar()'))
+    expect_equal(skip_repeated(stack), c('foo()', 'f()', ellip_h, 'f()', 'bar()'))
 })
 
 test_that('skip_repeated does not skip three or less consecutive items', {
