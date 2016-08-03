784cbf932b25dd582831cceac39ac8728ef1ef57
Philipp A <flying-sheep@web.de>
2016-08-03 10:40:53 +0200

    style

--- tests/testthat/test_kernel.r
+++ tests/testthat/test_kernel.r
@@ -1,7 +1,7 @@
 context('kernel')
 
-status <- system2('python3', c('-W', 'ignore::DeprecationWarning', 'test_ir.py'),
-                  wait=TRUE)
+status <- system2('python3', c('-W', 'ignore::DeprecationWarning', 'test_ir.py'), wait = TRUE)
+
 test_that('kernel tests pass', {
     expect_equal(status, 0)
 })
